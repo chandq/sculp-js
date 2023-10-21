9785d87ce97211cce0dec940137f932627c2c59f
chendq <deqiaochen@foxmail.com>
2023-10-21 16:41:23 +0800

    chore: change method of import module

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
-export default {
+module.exports = {
   collectCoverageFrom: ['src/**/*.ts'],
   testMatch: ['<rootDir>/test/**/*.test.ts'],
 
