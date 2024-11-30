7262ad7041929daf7924163a53202ff4d73dafe2
chendq <deqiaochen@foxmail.com>
2024-11-30 11:39:19 +0800

    feat: add cross domain download utils func

--- src/download.ts
+++ src/download.ts
@@ -1,5 +1,5 @@
 import { LooseParams } from './qs';
-import { AnyObject } from './type';
+import { AnyObject, isFunction } from './type';
 import { urlSetParams } from './url';
 
 /**
@@ -15,27 +15,54 @@ export function downloadURL(url: string, params?: LooseParams): void {
  * 通过 A 链接的方式下载
  * @param {string} href
  * @param {string} filename
+ * @param {Function} callback
  */
-export function downloadHref(href: string, filename: string): void {
+export function downloadHref(href: string, filename: string, callback?: Function): void {
   const eleLink = document.createElement('a');
   eleLink.download = filename;
   eleLink.style.display = 'none';
   eleLink.href = href;
   document.body.appendChild(eleLink);
   eleLink.click();
-  setTimeout(() => document.body.removeChild(eleLink));
+  setTimeout(() => {
+    document.body.removeChild(eleLink);
+    if (isFunction(callback)) {
+      callback();
+    }
+  });
 }
 
 /**
  * 将大文件对象通过 A 链接的方式下载
  * @param {Blob} blob
  * @param {string} filename
+ * @param {Function} callback
  */
-export function downloadBlob(blob: Blob, filename: string): void {
+export function downloadBlob(blob: Blob, filename: string, callback?: Function): void {
   const objURL = URL.createObjectURL(blob);
 
   downloadHref(objURL, filename);
-  setTimeout(() => URL.revokeObjectURL(objURL));
+  setTimeout(() => {
+    URL.revokeObjectURL(objURL);
+    if (isFunction(callback)) {
+      callback();
+    }
+  });
+}
+/**
+ * 根据URL下载文件（解决跨域a.download不生效问题)
+ * @param {string} url
+ * @param {string} filename
+ * @param {Function} callback
+ */
+export function crossDomainDownload(url: string, filename: string, callback?: Function): void {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.responseType = 'blob';
+  xhr.onload = function () {
+    if (xhr.status === 200) downloadBlob(xhr.response, filename, callback);
+  };
+  xhr.send();
 }
 
 export type FileType = 'json' | 'csv' | 'xls' | 'xlsx';
