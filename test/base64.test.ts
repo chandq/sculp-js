d7f8d5bc7308f0bce4007f62988bb7aee21d8e43
chendq <deqiaochen@foxmail.com>
2025-02-23 11:21:00 +0800

    test: update base64 unit test

--- test/base64.test.ts
+++ test/base64.test.ts
@@ -1,10 +1,10 @@
 import { decodeFromBase64, encodeToBase64 } from '../src/base64';
 
 test('encodeToBase64,decodeFromBase64', () => {
-  const rawStr = 'hello, #1Link-  欢迎来到前端JS世界&You like it?';
+  const rawStr = '"https://www.runoob.com/"::hello, #1Link-  欢迎来到前端JS世界&You like it?';
   const encodeStr = encodeToBase64(rawStr);
   expect(encodeStr).toBe(
-    'aGVsbG8lMkMlMjAlMjMxTGluay0lMjAlMjAlRTYlQUMlQTIlRTglQkYlOEUlRTYlOUQlQTUlRTUlODglQjAlRTUlODklOEQlRTclQUIlQUZKUyVFNCVCOCU5NiVFNyU5NSU4QyUyNllvdSUyMGxpa2UlMjBpdCUzRg=='
+    'JTIyaHR0cHMlM0ElMkYlMkZ3d3cucnVub29iLmNvbSUyRiUyMiUzQSUzQWhlbGxvJTJDJTIwJTIzMUxpbmstJTIwJTIwJUU2JUFDJUEyJUU4JUJGJThFJUU2JTlEJUE1JUU1JTg4JUIwJUU1JTg5JThEJUU3JUFCJUFGSlMlRTQlQjglOTYlRTclOTUlOEMlMjZZb3UlMjBsaWtlJTIwaXQlM0Y='
   );
   const decodeStr = decodeFromBase64(encodeStr);
   expect(decodeStr).toBe(rawStr);
