1f14c46abadc90f2a7157c018fa3a82972951e6e
chendq <deqiaochen@foxmail.com>
2025-06-01 19:02:47 +0800

    fix(file): improve compressImg, support large image

--- src/file.ts
+++ src/file.ts
@@ -1,3 +1,4 @@
+import { isObject } from './type';
 import { weAtob } from './we-decode';
 
 /**
@@ -41,23 +42,100 @@ export interface ICompressOptions {
 }
 
 /**
+ * 计算图片压缩后的尺寸
+ *
+ * @param {number} maxWidth
+ * @param {number} maxHeight
+ * @param {number} originWidth
+ * @param {number} originHeight
+ * @returns {*}
+ */
+function calculateSize({
+  maxWidth,
+  maxHeight,
+  originWidth,
+  originHeight
+}: {
+  maxWidth: number;
+  maxHeight: number;
+  originWidth: number;
+  originHeight: number;
+}): { width: number; height: number } {
+  let width = originWidth,
+    height = originHeight;
+  // 图片尺寸超过限制
+  if (originWidth > maxWidth || originHeight > maxHeight) {
+    if (originWidth / originHeight > maxWidth / maxHeight) {
+      // 更宽，按照宽度限定尺寸
+      width = maxWidth;
+      height = Math.round(maxWidth * (originHeight / originWidth));
+    } else {
+      height = maxHeight;
+      width = Math.round(maxHeight * (originWidth / originHeight));
+    }
+  }
+  return { width, height };
+}
+
+/**
+ * 根据原始图片的不同尺寸计算等比例缩放后的宽高尺寸
+ *
+ * @param {number} sizeKB
+ * @param {number} originWidth
+ * @param {number} originHeight
+ * @returns {*}
+ */
+function scalingByAspectRatio({ sizeKB, originWidth, originHeight }): { width: number; height: number } {
+  let targetWidth = originWidth,
+    targetHeight = originHeight;
+  if (1 * 1024 <= sizeKB && sizeKB < 10 * 1024) {
+    // [1MB, 10MB)
+    const maxWidth = 1600,
+      maxHeight = 1600;
+    const { width, height } = calculateSize({ maxWidth, maxHeight, originWidth, originHeight });
+    targetWidth = width;
+    targetHeight = height;
+  } else if (10 * 1024 <= sizeKB) {
+    // [10MB, Infinity)
+    const maxWidth = originWidth > 15000 ? 8192 : originWidth > 10000 ? 4096 : 2000,
+      maxHeight = originHeight > 15000 ? 8192 : originHeight > 10000 ? 4096 : 2000;
+
+    const { width, height } = calculateSize({ maxWidth, maxHeight, originWidth, originHeight });
+    targetWidth = width;
+    targetHeight = height;
+  }
+
+  return { width: targetWidth, height: targetHeight };
+}
+
+/**
  * Web端：等比例压缩图片批量处理 (size小于200KB，不压缩)
- * @param {File | FileList} file 文件
- * @param {ICompressOptions} options
+ *
+ * @param {File | FileList} file 图片或图片数组
+ * @param {ICompressOptions} options 压缩图片配置项，default: {quality:0.52,mime:'image/jpeg'}
  * @returns {Promise<object> | undefined}
  */
-export function compressImg(file: File | FileList, options: ICompressOptions): Promise<object> | undefined {
-  console.assert(file instanceof File || file instanceof FileList, `${file} 必须是File或FileList类型`);
-  console.assert(supportCanvas(), `当前环境不支持 Canvas`);
-  let targetQuality = 0.52;
+export function compressImg(
+  file: File | FileList,
+  options: ICompressOptions = { quality: 0.52, mime: 'image/jpeg' }
+): Promise<object> | undefined {
+  if (!(file instanceof File || file instanceof FileList)) {
+    throw new Error(`${file} require be File or FileList`);
+  } else if (!supportCanvas()) {
+    throw new Error(`Current runtime environment not support Canvas`);
+  }
+  const { quality = 0.52, mime = 'image/jpeg' }: ICompressOptions = isObject(options) ? options : {};
+  let targetQuality = quality;
   if (file instanceof File) {
     const sizeKB = +parseInt((file.size / 1024).toFixed(2));
     if (sizeKB < 1 * 1024) {
       targetQuality = 0.85;
     } else if (sizeKB >= 1 * 1024 && sizeKB < 5 * 1024) {
       targetQuality = 0.62;
-    } else if (sizeKB >= 5 * 1024) {
-      targetQuality = 0.52;
+    } else if (sizeKB >= 5 * 1024 && sizeKB < 10 * 1024) {
+      targetQuality = 0.75;
+    } else if (sizeKB >= 10 * 1024) {
+      targetQuality = 0.92;
     }
   }
   if (options.quality) {
@@ -80,49 +158,16 @@ export function compressImg(file: File | FileList, options: ICompressOptions): P
           image.onload = () => {
             const canvas = document.createElement('canvas'); // 创建 canvas 元素
             const context = canvas.getContext('2d');
-            let targetWidth = image.width;
-            let targetHeight = image.height;
             const originWidth = image.width;
             const originHeight = image.height;
-            if (1 * 1024 <= sizeKB && sizeKB < 10 * 1024) {
-              const maxWidth = 1600,
-                maxHeight = 1600;
-              targetWidth = originWidth;
-              targetHeight = originHeight;
-              // 图片尺寸超过的限制
-              if (originWidth > maxWidth || originHeight > maxHeight) {
-                if (originWidth / originHeight > maxWidth / maxHeight) {
-                  // 更宽，按照宽度限定尺寸
-                  targetWidth = maxWidth;
-                  targetHeight = Math.round(maxWidth * (originHeight / originWidth));
-                } else {
-                  targetHeight = maxHeight;
-                  targetWidth = Math.round(maxHeight * (originWidth / originHeight));
-                }
-              }
-            }
-            if (10 * 1024 <= sizeKB && sizeKB <= 20 * 1024) {
-              const maxWidth = 1400,
-                maxHeight = 1400;
-              targetWidth = originWidth;
-              targetHeight = originHeight;
-              // 图片尺寸超过的限制
-              if (originWidth > maxWidth || originHeight > maxHeight) {
-                if (originWidth / originHeight > maxWidth / maxHeight) {
-                  // 更宽，按照宽度限定尺寸
-                  targetWidth = maxWidth;
-                  targetHeight = Math.round(maxWidth * (originHeight / originWidth));
-                } else {
-                  targetHeight = maxHeight;
-                  targetWidth = Math.round(maxHeight * (originWidth / originHeight));
-                }
-              }
-            }
-            canvas.width = targetWidth;
-            canvas.height = targetHeight;
-            context!.clearRect(0, 0, targetWidth, targetHeight);
-            context!.drawImage(image, 0, 0, targetWidth, targetHeight); // 绘制 canvas
-            const canvasURL = canvas.toDataURL(options.mime, targetQuality);
+
+            const { width, height } = scalingByAspectRatio({ sizeKB, originWidth, originHeight });
+
+            canvas.width = width;
+            canvas.height = height;
+            context!.clearRect(0, 0, width, height);
+            context!.drawImage(image, 0, 0, width, height); // 绘制 canvas
+            const canvasURL = canvas.toDataURL(mime, targetQuality);
             const buffer = weAtob(canvasURL.split(',')[1]);
             let length = buffer.length;
             const bufferArray = new Uint8Array(new ArrayBuffer(length));
