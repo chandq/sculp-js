7fa7d16df43d689963171b257c542bc21f0157bb
chendq <deqiaochen@foxmail.com>
2026-07-14 11:10:38 +0800

    build: fix issue of build docs site failed

--- scripts/gen-docs-site-api.js
+++ scripts/gen-docs-site-api.js
@@ -3,6 +3,7 @@ const shelljs = require('shelljs');
 // shelljs.exec('api-extractor init');
 shelljs.rm('-rf', 'api-docs/markdown/*');
 shelljs.exec('api-extractor run --local --verbose');
+shelljs.mkdir('-p', 'api-docs/input');
 shelljs.cp('-R', 'temp/sculp-js.api.json', 'api-docs/input/');
 shelljs.cd('api-docs');
 shelljs.exec('api-documenter markdown');
